f73d4d7c3869acfcadc8645a75d16ffd57e4698e
ColbyCabrera <gdthyispro@gmail.com>
2025-10-05 01:42:29 -0400

    Fix unwanted padding under navbar in DeckPickerScreen.kt

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/DeckPickerScreen.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/DeckPickerScreen.kt
@@ -383,7 +383,7 @@ fun DeckPickerScreen(
                 onRebuild = onRebuild,
                 onEmpty = onEmpty,
                 listState = listState,
-                modifier = Modifier.padding(paddingValues)
+                contentPadding = paddingValues
             )
         }
         Scrim(
