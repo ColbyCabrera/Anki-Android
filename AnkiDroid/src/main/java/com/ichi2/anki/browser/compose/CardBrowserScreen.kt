722bdd44d78f8632aceb4ab07507d09748dcc149
ColbyCabrera <gdthyispro@gmail.com>
2025-10-24 19:43:20 -0400

    refactor: Simplify property access in CardBrowserScreen
    This commit simplifies the code in the `CardBrowserScreen` composable by directly accessing the `cellsList` and `text` properties of the `row` and `cell` objects, respectively.
    
    This removes the unnecessary use of getter methods (`getCellsList()` and `getText()`), resulting in more idiomatic and readable Kotlin code. A copyright header was also added.

--- AnkiDroid/src/main/java/com/ichi2/anki/browser/compose/CardBrowserScreen.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/browser/compose/CardBrowserScreen.kt
@@ -1,3 +1,18 @@
+/***************************************************************************************
+ * Copyright (c) 2022 Ankitects Pty Ltd <https://apps.ankiweb.net>                      *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
 package com.ichi2.anki.browser.compose
 
 import androidx.compose.foundation.clickable
@@ -103,9 +118,9 @@ fun CardBrowserRow(
     ) {
         // This is a simplified representation. The original adapter handles HTML.
         // For a full conversion, a composable that can render HTML would be needed.
-        row.getCellsList().forEach { cell ->
+        row.cellsList.forEach { cell ->
             Text(
-                text = cell.getText(),
+                text = cell.text,
                 style = MaterialTheme.typography.bodyMedium,
                 maxLines = 1,
                 modifier = Modifier.weight(1f) // Basic weighting
