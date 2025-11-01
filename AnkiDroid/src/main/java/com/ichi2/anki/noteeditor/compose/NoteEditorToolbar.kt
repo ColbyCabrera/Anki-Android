1aaf1174af05f3e5cbe2512f0e91706451586a81
ColbyCabrera <gdthyispro@gmail.com>
2025-10-31 21:03:55 -0400

    chore(NoteEditor): Remove unused import
    This commit removes an unused import for `FilledTonalIconButton` from the `NoteEditorToolbar.kt` file.

--- AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/compose/NoteEditorToolbar.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/compose/NoteEditorToolbar.kt
@@ -36,7 +36,6 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
