ac92d4b7ba880cab34f90009dcb02543694ea9a9
ColbyCabrera <gdthyispro@gmail.com>
2025-11-01 20:16:45 -0400

    chore(NoteEditor): Remove unused imports
    This commit removes unused imports from `NoteEditorTopBar.kt`.
    
    The following imports have been removed:
    - `androidx.compose.material.icons.automirrored.filled.ArrowBack`
    - `androidx.compose.material.icons.filled.RemoveRedEye`
    - `androidx.compose.material.icons.filled.Save`
    - `androidx.compose.material3.FilledTonalIconButton`
    - `androidx.compose.material3.IconButton`
    - `androidx.compose.ui.graphics.vector.ImageVector`

--- AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/compose/NoteEditorTopBar.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/noteeditor/compose/NoteEditorTopBar.kt
@@ -22,10 +22,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.RemoveRedEye
-import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
@@ -34,9 +31,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -45,12 +40,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
