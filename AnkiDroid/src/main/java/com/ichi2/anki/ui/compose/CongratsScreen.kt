81456349ff64d4b2fb1f38c50ded7984d92299dd
ColbyCabrera <gdthyispro@gmail.com>
2025-11-11 23:10:26 -0500

    refactor(CongratsScreen): Remove unused import
    This commit removes the unused import for `FilledTonalIconButton` from the `CongratsScreen.kt` file.

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/CongratsScreen.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/CongratsScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
