ad0aa03e17e113daa8bb21c11213cfbad80b723f
ColbyCabrera <gdthyispro@gmail.com>
2025-11-21 23:24:18 -0500

    style(AnkiDroidApp): Reduce bottom padding in search UI
    This commit decreases the bottom padding of the search component in `AnkiDroidApp.kt` from `16.dp` to `8.dp` to adjust the vertical spacing.

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/AnkiDroidApp.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/AnkiDroidApp.kt
@@ -230,7 +230,7 @@ fun AnkiDroidApp(
                                     onExpandedChange = { },
                                     modifier = Modifier
                                         .weight(1f)
-                                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                                        .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
                                         .graphicsLayer {
                                             alpha = searchAnim
                                         },
