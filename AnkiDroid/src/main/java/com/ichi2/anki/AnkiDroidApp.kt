6fe2fc2be9e879bc31c83b96afaa9489ff314d56
ColbyCabrera <gdthyispro@gmail.com>
2025-12-27 23:27:28 -0500

    Refactor: Remove unused `CompatHelper` import
    This commit removes an unused import for `CompatHelper` from the `AnkiDroidApp` class.

--- AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/AnkiDroidApp.kt
@@ -56,7 +56,6 @@ import com.ichi2.anki.servicelayer.ThrowableFilterService
 import com.ichi2.anki.services.NotificationService
 import com.ichi2.anki.settings.Prefs
 import com.ichi2.anki.ui.dialogs.ActivityAgnosticDialogs
-import com.ichi2.compat.CompatHelper
 import com.ichi2.utils.AdaptionUtil
 import com.ichi2.utils.ExceptionUtil
 import com.ichi2.utils.LanguageUtil
