5a1ad1e495e231a44e97636f8fe0b0cba931678b
ColbyCabrera <gdthyispro@gmail.com>
2025-12-28 22:52:16 -0500

    Refactor(tests): Move whiteboard color tests to `ReviewerNoParamTest`
    This commit moves the whiteboard pen color persistence tests from `ReviewerTest` to `ReviewerNoParamTest`.
    
    The original `ReviewerTest` is parameterized, causing these data-layer tests to run multiple times unnecessarily. Moving them to the non-parameterized `ReviewerNoParamTest` ensures they run only once, improving test suite efficiency.
    
    All unrelated tests concerning gestures, fullscreen mode, and drawer conflicts have been removed from `ReviewerNoParamTest`, leaving it solely focused on verifying the whiteboard pen color logic at the `MetaDB` layer.

--- AnkiDroid/src/test/java/com/ichi2/anki/ReviewerNoParamTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/ReviewerNoParamTest.kt
@@ -15,51 +15,30 @@
  */
 package com.ichi2.anki
 
-import android.content.Intent
 import android.graphics.Color
-import androidx.annotation.CheckResult
-import androidx.core.content.edit
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import anki.scheduler.CardAnswer.Rating
-import com.ichi2.anki.cardviewer.Gesture
-import com.ichi2.anki.cardviewer.Gesture.SWIPE_DOWN
-import com.ichi2.anki.cardviewer.Gesture.SWIPE_RIGHT
-import com.ichi2.anki.cardviewer.Gesture.SWIPE_UP
-import com.ichi2.anki.cardviewer.GestureProcessor
-import com.ichi2.anki.cardviewer.ViewerCommand
 import com.ichi2.anki.libanki.Consts
 import com.ichi2.anki.libanki.DeckId
 import com.ichi2.anki.model.WhiteboardPenColor
-import com.ichi2.anki.preferences.sharedPrefs
-import com.ichi2.anki.reviewer.Binding
-import com.ichi2.anki.reviewer.FullScreenMode
-import com.ichi2.anki.reviewer.FullScreenMode.Companion.setPreference
-import com.ichi2.anki.reviewer.MappableBinding.Companion.toPreferenceString
-import com.ichi2.anki.reviewer.ReviewerBinding
-import com.ichi2.anki.utils.ext.addBinding
-import com.ichi2.testutils.common.Flaky
-import com.ichi2.testutils.common.OS
-import com.ichi2.themes.Theme
-import com.ichi2.themes.Themes.currentTheme
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.greaterThan
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 
-/** A non-parameterized ReviewerTest - we should probably rename ReviewerTest in future  */
+/**
+ * Tests for whiteboard pen color persistence in MetaDB.
+ * These tests verify the data layer behavior, not UI behavior.
+ */
 @RunWith(AndroidJUnit4::class)
 class ReviewerNoParamTest : RobolectricTest() {
-    override fun getCollectionStorageMode() = CollectionStorageMode.IN_MEMORY_WITH_MEDIA
-
     @Before
     override fun setUp() {
         super.setUp()
-        // This doesn't do an upgrade in the correct place
         MetaDB.resetDB(targetContext)
     }
+
+    // region Whiteboard Pen Color Tests (MetaDB layer)
 
     @Test
     fun defaultWhiteboardColorIsUsedOnFirstRun() {
@@ -119,204 +98,9 @@ class ReviewerNoParamTest : RobolectricTest() {
         assertThat("Pen color for default deck is null (use UI default)", retrievedColor.lightPenColor, equalTo(null))
     }
 
-    @Test
-    fun flippingCardHidesFullscreen() {
-        addBasicNote("Hello", "World")
-        val reviewer = startReviewerFullScreen()
+    // endregion
 
-        val hideCount = reviewer.delayedHideCount
-
-        reviewer.displayCardAnswer()
-
-        assertThat("Hide should be called after flipping a card", reviewer.delayedHideCount, greaterThan(hideCount))
-    }
-
-    @Test
-    @Flaky(
-        OS.ALL,
-        "Hide should be called after answering a card" +
-            "    Expected: a value greater than <2>" +
-            "         but: <2> was equal to <2>",
-    )
-    fun showingCardHidesFullScreen() {
-        addBasicNote("Hello", "World")
-        val reviewer = startReviewerFullScreen()
-
-        reviewer.displayCardAnswer()
-        advanceRobolectricLooper()
-
-        val hideCount = reviewer.delayedHideCount
-
-        reviewer.answerCard(Rating.AGAIN)
-        advanceRobolectricLooper()
-
-        assertThat("Hide should be called after answering a card", reviewer.delayedHideCount, greaterThan(hideCount))
-    }
-
-    @Test
-    @Flaky(OS.ALL, "Expected: a value greater than <2> but: <2> was equal to <2>")
-    fun undoingCardHidesFullScreen() =
-        runTest {
-            addBasicNote("Hello", "World")
-            val reviewer = startReviewerFullScreen()
-
-            reviewer.displayCardAnswer()
-            advanceRobolectricLooper()
-            reviewer.answerCard(Rating.AGAIN)
-            advanceRobolectricLooper()
-
-            val hideCount = reviewer.delayedHideCount
-
-            reviewer.undo()
-
-            advanceRobolectricLooper()
-
-            assertThat("Hide should be called after answering a card", reviewer.delayedHideCount, greaterThan(hideCount))
-        }
-
-    @Test
-    @Flaky(OS.ALL, "hasDrawerSwipeConflicts was false")
-    fun defaultDrawerConflictIsTrueIfGesturesEnabled() {
-        enableGestureSetting()
-        enableGesture(SWIPE_RIGHT)
-        val reviewer = startReviewerFullScreen()
-
-        assertThat(reviewer.hasDrawerSwipeConflicts(), equalTo(true))
-    }
-
-    @Test
-    fun noDrawerConflictsBeforeOnCreate() {
-        enableGestureSetting()
-        val controller = Robolectric.buildActivity(Reviewer::class.java, Intent())
-        try {
-            assertThat("no conflicts before onCreate", controller.get().hasDrawerSwipeConflicts(), equalTo(false))
-        } finally {
-            try {
-                enableGesture(SWIPE_UP)
-            } catch (e: Exception) {
-                // ignore
-            }
-        }
-    }
-
-    @Test
-    fun noDrawerConflictsIfGesturesDisabled() {
-        disableGestureSetting()
-        enableGesture(SWIPE_UP)
-        val reviewer = startReviewerFullScreen()
-        assertThat("gestures should be disabled", gestureProcessor.isEnabled, equalTo(false))
-        assertThat(reviewer.hasDrawerSwipeConflicts(), equalTo(false))
-    }
-
-    @Test
-    fun noDrawerConflictsIfNoGestures() {
-        enableGestureSetting()
-        disableConflictGestures()
-        val reviewer = startReviewerFullScreen()
-        assertThat("gestures should be enabled", gestureProcessor.isEnabled, equalTo(true))
-        assertThat("no conflicts, so no conflicts detected", reviewer.hasDrawerSwipeConflicts(), equalTo(false))
-    }
-
-    @Test
-    @Flaky(os = OS.ALL, "final assertion is false")
-    fun drawerConflictsIfUp() {
-        enableGestureSetting()
-        disableConflictGestures()
-        enableGesture(SWIPE_UP)
-        val reviewer = startReviewerFullScreen()
-        assertThat("gestures should be enabled", gestureProcessor.isEnabled, equalTo(true))
-        assertThat(reviewer.hasDrawerSwipeConflicts(), equalTo(true))
-    }
-
-    @Test
-    @Flaky(os = OS.ALL, "final assertion is false")
-    fun drawerConflictsIfDown() {
-        enableGestureSetting()
-        disableConflictGestures()
-        enableGesture(SWIPE_DOWN)
-        val reviewer = startReviewerFullScreen()
-        assertThat("gestures should be enabled", gestureProcessor.isEnabled, equalTo(true))
-        assertThat(reviewer.hasDrawerSwipeConflicts(), equalTo(true))
-    }
-
-    @Test
-    @Flaky(os = OS.ALL, "final assertion is false")
-    fun drawerConflictsIfRight() {
-        enableGestureSetting()
-        disableConflictGestures()
-        enableGesture(SWIPE_RIGHT)
-        val reviewer = startReviewerFullScreen()
-        assertThat("gestures should be enabled", gestureProcessor.isEnabled, equalTo(true))
-        assertThat(reviewer.hasDrawerSwipeConflicts(), equalTo(true))
-    }
-
-    @Test
-    fun normalReviewerFitsSystemWindows() {
-        val reviewer = startReviewer()
-        assertThat(reviewer.fitsSystemWindows(), equalTo(true))
-    }
-
-    @Test
-    fun fullscreenDoesNotFitSystemWindow() {
-        val reviewer = startReviewerFullScreen()
-        assertThat(reviewer.fitsSystemWindows(), equalTo(false))
-    }
-
-    private val gestureProcessor: GestureProcessor
-        get() {
-            val gestureProcessor = GestureProcessor(null)
-            gestureProcessor.init(targetContext.sharedPrefs())
-            return gestureProcessor
-        }
-
-    private fun disableConflictGestures() {
-        disableGestures(SWIPE_UP, SWIPE_DOWN, SWIPE_RIGHT)
-    }
-
-    private fun enableGestureSetting() {
-        setGestureSetting(true)
-    }
-
-    private fun disableGestureSetting() {
-        setGestureSetting(false)
-    }
-
-    private fun setGestureSetting(value: Boolean) {
-        targetContext.sharedPrefs().edit {
-            putBoolean(GestureProcessor.PREF_KEY, value)
-        }
-    }
-
-    private fun disableGestures(vararg gestures: Gesture) {
-        val prefs = targetContext.sharedPrefs()
-        for (command in ViewerCommand.entries) {
-            for (mappableBinding in ReviewerBinding.fromPreference(prefs, command)) {
-                val gestureBinding = mappableBinding.binding as? Binding.GestureInput? ?: continue
-                if (gestureBinding.gesture in gestures) {
-                    val bindings = ReviewerBinding.fromPreferenceString(command.preferenceKey).toMutableList()
-                    bindings.remove(mappableBinding)
-                    prefs.edit {
-                        putString(command.preferenceKey, bindings.toPreferenceString())
-                    }
-                }
-            }
-        }
-    }
-
-    /** Enables a gesture (without changing the overall setting of whether gestures are allowed)  */
-    private fun enableGesture(gesture: Gesture) {
-        val prefs = targetContext.sharedPrefs()
-        ViewerCommand.FLIP_OR_ANSWER_EASE1.addBinding(
-            prefs,
-            ReviewerBinding.fromGesture(gesture),
-        )
-    }
-
-    private fun startReviewerFullScreen(): ReviewerExt {
-        val sharedPrefs = targetContext.sharedPrefs()
-        setPreference(sharedPrefs, FullScreenMode.BUTTONS_ONLY)
-        return ReviewerTest.startReviewer(this, ReviewerExt::class.java)
-    }
+    // region Helper Methods
 
     @Suppress("SameParameterValue")
     private fun storeDarkModeColor(value: Int) {
@@ -324,10 +108,7 @@ class ReviewerNoParamTest : RobolectricTest() {
     }
 
     @Suppress("SameParameterValue")
-    private fun storeLightModeColor(
-        value: Int,
-        did: DeckId?,
-    ) {
+    private fun storeLightModeColor(value: Int, did: DeckId?) {
         MetaDB.storeWhiteboardPenColor(targetContext, did!!, false, value)
     }
 
@@ -339,18 +120,7 @@ class ReviewerNoParamTest : RobolectricTest() {
     private val penColor: WhiteboardPenColor
         get() = MetaDB.getWhiteboardPenColor(targetContext, Consts.DEFAULT_DECK_ID)
 
-
-
-    private fun startReviewer(): Reviewer = ReviewerTest.startReviewer(this)
-
-    private class ReviewerExt : Reviewer() {
-        var delayedHideCount = 0
-
-        override fun delayedHide(delayMillis: Int) {
-            delayedHideCount++
-            super.delayedHide(delayMillis)
-        }
-    }
+    // endregion
 
     companion object {
         const val DEFAULT_LIGHT_PEN_COLOR = Color.BLACK
